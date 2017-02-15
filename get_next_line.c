cfda875dd6982f731c4da23e86af07df19f50f00
Irraky <drecours@student.42.fr>
2017-02-15 22:15:24 +0100

    brouillon

--- get_next_line.c
+++ get_next_line.c
@@ -14,5 +14,24 @@
 
 int		get_next_line(const int fd, char **line)
 {
-	return (ERROR);
+	char *buff[BUFF_SIZE + 1];
+	char	*tmp;
+	int	flag;
+	
+	if (fd < 0 || !line)
+		return (ERROR);
+	while (flag)
+	{
+		strcpy line -> tmp
+		ft_bzero(buff);
+		je lis fd dans le buff 
+		je fou les trucs d'erreur
+		si 0 caract dedans
+			return (END_OF_FILE);
+		je cherche un \n (incrementation variable i)
+		line = memalloc taille de line = i = 1
+		strcpy tmp -> line 
+			strcmp de buff jusqu au \n a la fin de line
+	}
+	return (E_SUCCESS);
 }
